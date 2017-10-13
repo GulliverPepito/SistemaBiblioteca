02c4ad2e143bb9366975ec00e9d9b4be0bc9b56b
Mi nombre <jonipeque@hotmail.com>
2017-10-13 12:19:02 -0500

    Menu

---
+++ js/menu.js
@@ -0,0 +1,47 @@
+$(document).ready(function (){
+       $('.libros').hide();
+       $('.prestamos').hide();
+       $('.consultas').hide();
+        $('.usuarios').hide();
+         var li = 0, p = 0, c=0, u=0;
+       $('#libros').click(function(){
+           if(li==0){
+               $('.libros').slideDown("slow");
+               li=1;
+           }else{
+               $('.libros').hide("slow");
+               li = 0;
+           }
+          
+       });
+        $('#prestamos').click(function(){
+           if(p==0){
+               $('.prestamos').slideDown("slow");
+               p=1;
+           }else{
+               $('.prestamos').hide("slow");
+               p = 0;
+           }
+          
+       });
+        $('#consultas').click(function(){
+           if(c==0){
+               $('.consultas').slideDown("slow");
+               c=1;
+           }else{
+               $('.consultas').hide("slow");
+               c = 0;
+           }
+          
+       });
+        $('#usuarios').click(function(){
+           if(u==0){
+               $('.usuarios').slideDown("slow");
+               u=1;
+           }else{
+               $('.usuarios').hide("slow");
+               u = 0;
+           }
+          
+       });
+    });
