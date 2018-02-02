a5399f01e22f19de0608efc8c3003fcb46d72cca
juan <jramongarciaangel@gmail.com>
2018-02-01 20:13:43 -0600

    front Usuarios

---
+++ js/usuarios.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#AgreUsu").click(function() {
+		$(".Dusu").hide();
+		$("#ReUsu").show();
+	});
+
+	$("#MoUsu").click(function() {
+		$(".Dusu").hide();
+		$("#VerUsu").show();
+	});
+});
