13d731b5ed84e52b03d93fca4a3707d2d5394de5
JohnMontana <jonipeque@hotmail.com>
2018-02-26 11:14:14 -0600

    Vistas

---
+++ vistas/editarLibro.php
@@ -0,0 +1,39 @@
+<div class="card">
+                <div class="col-md-12">
+                    <div class="card-body">
+                        <div class="input-group">
+                            <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-search"></span></span>
+                            <input class="form-control input-lg" type="text" id="BuscaLibro" placeholder="Buscar libros">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div id="TLibros">
+                            <table class="table table-hover table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th>ISBN</th>
+                                        <th>Código Barras</th>
+                                        <th>Título</th>
+                                        <th>Autor</th>
+                                        <th>Título Original</th>
+                                        <th>Año Edición</th>
+                                        <th>Lugar Edición</th>
+                                        <th>Editorial</th>
+                                        <th>Páginas</th>
+                                        <th>Ubicación</th>
+                                        <th>Volúmen</th>
+                                        <th>N° Serie</th>
+                                        <th>Carrera</th>
+                                        <th>URL</th>
+                                        <th>Tema Gral</th>
+                                        <th>Tema Esp</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="datos">
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
