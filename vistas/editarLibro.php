5db358d4023eac0a206f8239108b2bf02e3cc771
JohnMontana <jonipeque@hotmail.com>
2018-02-27 12:09:42 -0600

    Editar Libro

--- vistas/editarLibro.php
+++ vistas/editarLibro.php
@@ -1,39 +1,90 @@
 <div class="card">
+    <div class="card-body">
+    <div class="row">
                 <div class="col-md-12">
-                    <div class="card-body">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-search"></span></span>
+                    <div class="input-group">
+                            <span class="input-group-addon" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
                             <input class="form-control input-lg" type="text" id="BuscaLibro" placeholder="Buscar libros">
-                        </div>
                     </div>
+                </div>
+        </div>
+        <div class="row">
                     <div class="col-md-12">
-                        <div id="TLibros">
-                            <table class="table table-hover table-responsive">
+                        <div id="TLibros" class="tabla">
+                            <table class="table">
                                 <thead>
                                     <tr>
-                                        <th>ISBN</th>
-                                        <th>Código Barras</th>
-                                        <th>Título</th>
-                                        <th>Autor</th>
-                                        <th>Título Original</th>
-                                        <th>Año Edición</th>
-                                        <th>Lugar Edición</th>
+                                        <th scope="col">ISBN</th>
+                                        <th scope="col">Código Barras</th>
+                                        <th scope="col">Título</th>
+                                        <th scope="col">Autor</th>
+                                        <th scope="col">Título Original</th>
                                         <th>Editorial</th>
                                         <th>Páginas</th>
                                         <th>Ubicación</th>
-                                        <th>Volúmen</th>
-                                        <th>N° Serie</th>
                                         <th>Carrera</th>
-                                        <th>URL</th>
-                                        <th>Tema Gral</th>
-                                        <th>Tema Esp</th>
                                     </tr>
                                 </thead>
                                 <tbody id="datos">
-
+                                    <tr>
+                                       <td colspan="12">
+                                        <div class="card tinto">
+                                            <div class="card-body ">
+                                                <div class="libro">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <p>ISNB:</p>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>Codigo Barras:</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <p>Titulo:</p>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>Autor:</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <p>Titulo Original:</p>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                             <p>Titulo Original:</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <p>Editorial:</p>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>Paginas:</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <p>Ubicacion:</p>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <p>Carrera:</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row justify-content-end">
+                                                        <div class="col-md-3">
+                                                            <button type="button" class="btn btn-warning"><i class="fa fa-plus" aria-hidden="true"></i> Ver Mas</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                       </td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
-                </div>
-            </div>
+                    </div>
+                </div>    
+</div>
