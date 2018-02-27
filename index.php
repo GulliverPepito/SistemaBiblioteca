74791790ac5f8ceb191baa65e99da4ab21cec02d
JohnMontana <jonipeque@hotmail.com>
2018-02-27 09:53:43 -0600

    Editar

--- index.php
+++ index.php
@@ -46,45 +46,7 @@
         </div>
 
         <div class="row mostrar" id="TablaLibros">
-            <div class="card">
-                <div class="col-md-12">
-                    <div class="card-body">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-search"></span></span>
-                            <input class="form-control input-lg" type="text" id="BuscaLibro" placeholder="Buscar libros">
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div id="TLibros">
-                            <table class="table table-hover table-responsive">
-                                <thead>
-                                    <tr>
-                                        <th>ISBN</th>
-                                        <th>Código Barras</th>
-                                        <th>Título</th>
-                                        <th>Autor</th>
-                                        <th>Título Original</th>
-                                        <th>Año Edición</th>
-                                        <th>Lugar Edición</th>
-                                        <th>Editorial</th>
-                                        <th>Páginas</th>
-                                        <th>Ubicación</th>
-                                        <th>Volúmen</th>
-                                        <th>N° Serie</th>
-                                        <th>Carrera</th>
-                                        <th>URL</th>
-                                        <th>Tema Gral</th>
-                                        <th>Tema Esp</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="datos">
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php require "vistas/editarLibro.php";?>
         </div>
     </section>
     <script src="js/jquery-3.1.1.min.js"></script>
