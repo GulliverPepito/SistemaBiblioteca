6ad6d0c5606a11c0564a5d14c57cb1a547ae0a0c
JohnMontana <jonipeque@hotmail.com>
2018-03-06 14:14:03 -0600

    Modificar Libro

--- php/modificarLibro.php
+++ php/modificarLibro.php
@@ -114,4 +114,22 @@
             }
         }
     }
+    if(isset($_POST) && $_POST['tag']=='actualizar'){
+        $isbn= mysqli_real_escape_string($con, $_POST['isbn']);
+        $codigoBarras = mysqli_real_escape_string($con, $_POST['codigoBarras']);
+        $titulo = mysqli_real_escape_string($con, $_POST['titulo']);
+        $autor = mysqli_real_escape_string($con, $_POST['autor']);
+        $tituloOriginal = mysqli_real_escape_string($con, $_POST['tituloOriginal']);
+        $anioEdicion = mysqli_real_escape_string($con, $_POST['anioEdicion']);
+        $lugarEdicion = mysqli_real_escape_string($con, $_POST['lugarEdicion']);
+        $editorial = mysqli_real_escape_string($con, $_POST['editorial']);
+        $paginas = mysqli_real_escape_string($con, $_POST['paginas']);
+        $ubicacion = mysqli_real_escape_string($con, $_POST['ubicacion']);
+        $volumen = mysqli_real_escape_string($con, $_POST['volumen']);
+        $numSerie = mysqli_real_escape_string($con, $_POST['numSerie']);
+        $carrera = mysqli_real_escape_string($con, $_POST['carrera']);
+        $url = mysqli_real_escape_string($con, $_POST['url']);
+        $temaEspecifico = mysqli_real_escape_string($con, $_POST['temaEspecifico']);
+        $temaGeneral = mysqli_real_escape_string($con, $_POST['temaGeneral']);
+    }
 ?>
