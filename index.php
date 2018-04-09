d0c6ef53bab4798684284575c8c278437531c1ac
JohnMontana <jonipeque@hotmail.com>
2018-04-09 12:11:13 -0500

    Prestamos

--- index.php
+++ index.php
@@ -53,6 +53,9 @@
             <div class="row mostrar" id="estadis">
                <?php require "vistas/estadisticas.php";?>
             </div>
+            <div class="row " id='nuevoPrestamo'>
+                <?php require "vistas/nuevoPrestamo.php";?>
+            </div>
         </div>
     </section>
     <script src="js/jquery-3.1.1.min.js"></script>
